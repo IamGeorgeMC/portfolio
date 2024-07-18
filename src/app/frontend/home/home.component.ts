e047a5e1af018d9912601d3bc76f7beaee3e5983
Jorge Luis Morales <iamgeorge@Jorges-MacBook-Air.local>
2024-07-18 12:09:03 -0600

    cambios en los componentes de redes sociales, home, y se agrega archivo cv

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SocialredComponent } from '../../components/socialred/socialred.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SocialredComponent],
+  imports: [SocialredComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
